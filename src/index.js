2b834f94e527512812eefdbce328a63827649ba0
Shyren <shyren.more30@gmail.com>
2020-11-01 11:21:24 +0530

    create array of obj, customised UI and components

--- src/index.js
+++ src/index.js
@@ -3,48 +3,83 @@ import ReactDOM from 'react-dom'
 import './index.css' 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
+// Array of objects 
+const bookArr = [
+	{
+		bookImg: 'https://images-eu.ssl-images-amazon.com/images/I/71NiPFoJx1L._UX300__PJku-sticker-v7,TopRight,0,-50__BG0,0,0,0_FMpng_AC_UL200_SR200,200_.jpg',
+		bookName: 'The Morning Star',
+		bookAuthor: 'Gita V.reddy',
+		bookPrice: 'Rs 293.00',
+	},
+	{
+		bookImg: 'https://images-eu.ssl-images-amazon.com/images/I/91n6oKUsMnL._UX300__PJku-sticker-v7,TopRight,0,-50__BG0,0,0,0_FMpng_AC_UL200_SR200,200_.jpg',
+		bookName: 'Lost: Darkness to Light',
+		bookAuthor: "Delton D'Souza",
+		bookPrice: 'Rs 99.00',
+	},
+	{
+		bookImg: 'https://images-eu.ssl-images-amazon.com/images/I/91n6oKUsMnL._UX300__PJku-sticker-v7,TopRight,0,-50__BG0,0,0,0_FMpng_AC_UL200_SR200,200_.jpg',
+		bookName: 'Lost: Darkness to Light',
+		bookAuthor: "Delton D'Souza",
+		bookPrice: 'Rs 99.00',
+	},
+	{
+		bookImg: 'https://images-eu.ssl-images-amazon.com/images/I/91n6oKUsMnL._UX300__PJku-sticker-v7,TopRight,0,-50__BG0,0,0,0_FMpng_AC_UL200_SR200,200_.jpg',
+		bookName: 'Lost: Darkness to Light',
+		bookAuthor: "Delton D'Souza",
+		bookPrice: 'Rs 99.00',
+	},
+]
+
+
 function Booklist() {
 	return (
-		<section className="container-fluid">
-			<div className="row mt-4">
-				<Book /> 
-				<Book />
+		<section className="container-fluid ">
+			<div className="row mt-4 justify-content-center">
+				
+				{/* 
+					Children props are everything that we render in between the opening and closing tag of the component
+					
+					Objects are not valid as react child props
+					
+					NOTE: that does NOT imply array elements cannot be passed as child props 
+
+					You can't pass array elements that are of type object 
+
+					other things like text, etc are allowed 
+				*/}
+				{
+					bookArr.map((book) => {
+						// console.log(book)
+
+						const {bookImg, bookName, bookAuthor, bookPrice} = book
+						return (
+							<Book book={book}></Book>
+						)
+					})
+				}
 			</div>
 		</section>
 	)
 }
 
-const Book = () => {
+const Book = (props) => {
+	// console.log(props);
+
+	// destructuring objects (props)
+	const { bookImg, bookName ,bookAuthor, bookPrice } = props.book // props.book, book is that mapping wala book
+
 	return (
-		<div className="col-sm-12 col-md-3 col-lg-4 text-center book">
-			<BookImg></BookImg>
-			<BookName></BookName>
-			<BookAuthor></BookAuthor>
-			<BookPriceINR></BookPriceINR>	
+		<div className="col-sm-12 col-md-3 text-center book">
+			<img src={bookImg} alt="" className="img-fluid" width="350px" height="300px" />
+			<h1>{bookName}</h1>
+			<h3 className="bookauthor">{bookAuthor}</h3>
+			<h3>{bookPrice}</h3>
+			{/* <p>{6+9}</p> */}
+
+			{props.children}
 		</div>
 	)
-}
-
-const BookName = () => {
-	return (
-		<h1>The Morning Star</h1>
-	)	
-}
-
-const BookAuthor = () => {
-	return (
-		<h3 className="bookauthor">Gita V.reddy</h3>
-	)
-}
-
-const BookPriceINR = () => {
-	return (
-		<h3>Rs 293.00</h3>
-	)
-}
-
-const BookImg = () => {
-	return <img src="https://images-eu.ssl-images-amazon.com/images/I/71NiPFoJx1L._UX300__PJku-sticker-v7,TopRight,0,-50__BG0,0,0,0_FMpng_AC_UL200_SR200,200_.jpg" alt="" width="400px"/>
 }
 
 
